6d0ac674c8ba95fe696664db8c120c506bbbdae5
boy7307979 <boy7302008@hotmail.com>
2018-05-18 01:59:29 +0800

    first commit

---
+++ Thirdparty/Trace/trace.cpp
@@ -0,0 +1,13 @@
+
+#include "trace.h"
+
+
+void Tracer::trace_init()
+{
+	atrace_marker_fd = -1;
+	atrace_marker_fd = open("/sys/kernel/debug/tracing/trace_marker", O_WRONLY);
+  if (atrace_marker_fd == -1)
+  { /* do error handling */
+	 printf("Error Opening Trace file");
+  }
+}
---
+++ Thirdparty/Trace/trace.h
@@ -0,0 +1,35 @@
+#ifndef TRACE_H
+#define TRACE_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define ATRACE_MESSAGE_LEN 256
+
+
+class Tracer {
+
+public:
+
+	static void trace_init();
+
+
+	static inline void trace_begin(const char *name)
+	{
+	    char buf[ATRACE_MESSAGE_LEN];
+	    int len = snprintf(buf, ATRACE_MESSAGE_LEN, "B|%d|%s", getpid(), name);
+	    write(atrace_marker_fd, buf, len);
+	}
+
+	static inline void trace_end()
+	{
+	    char c = 'E';
+	    write(atrace_marker_fd, &c, 1);
+	}
+
+	int atrace_marker_fd;
+};
+
+#endif
